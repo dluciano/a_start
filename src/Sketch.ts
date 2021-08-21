6a9c986e67ddbc02c6bf70601adf6c6aa86f8418
Dawlin Pena Luciano <dawlin@igiltd.com>
2021-08-21 05:28:05 +0100

    add multiple agents

--- src/Sketch.ts
+++ src/Sketch.ts
@@ -6,22 +6,10 @@ export const Sketch = (p5: P5) => {
   const cols = 50;
   const wallPerct = 0.2;
 
-  const startColIndex = Math.trunc(p5.random(0, cols - 1));
-  const startRowIndex = Math.trunc(p5.random(0, rows - 1));
-  const endColIndex = Math.trunc(p5.random(0, cols - 1));
-  const endRowIndex = Math.trunc(p5.random(0, rows - 1));
-
-  const grid = Grid(
-    p5,
-    cols,
-    rows,
-    { startColIndex, startRowIndex, endColIndex, endRowIndex },
-    wallPerct,
-    () => ({
-      width: p5.width,
-      height: p5.height,
-    })
-  );
+  const grid = Grid(p5, cols, rows, wallPerct, () => ({
+    width: p5.width,
+    height: p5.height,
+  }));
 
   p5.setup = () => {
     p5.createCanvas(700, 700);
--- src/components/Grid.ts
+++ src/components/Grid.ts
@@ -1,36 +1,57 @@
-import {
-  CellType,
-  ICell,
-  IRenderable,
-  ISize,
-  IWall,
-} from "./interfaces";
+import { CellType, ICell, IRenderable, ISize, IWall } from "./interfaces";
 import { ICellElement, ICellPathFinderData, PathFinder } from "../pathfinder";
 
 import { Drawer } from "./Drawer";
 import p5 from "p5";
 import { setNeighbors } from "./Cell";
 
+export const Agent = (p5: p5, datas: ICellPathFinderData[][]) => {
+  const validDatas = datas.flatMap((d) => d).filter((d) => d);
+
+  const start = validDatas[Math.trunc(p5.random(0, validDatas.length - 1))];
+  const end = validDatas[Math.trunc(p5.random(0, validDatas.length - 1))];
+
+  const result = PathFinder({
+    start: start!,
+    end: end!,
+  });
+
+  return {
+    ...result,
+    start,
+    end,
+  };
+};
+
+const createAgent = (p5: p5, datas: ICellPathFinderData[][]) => {
+  const result = Agent(p5, datas);
+
+  for (const cell of result.openSet) {
+    cell.element!.types = CellType.OpenSet;
+  }
+
+  for (const cell of result.closeSet) {
+    cell.element!.types = CellType.CloseSet;
+  }
+
+  result.end!.element!.types = CellType.Target;
+  return result.path.map((p) => p.element);
+};
+
 export const Grid = (
   p5: p5,
   cols: number,
   rows: number,
-  init: {
-    startColIndex: number;
-    startRowIndex: number;
-    endColIndex: number;
-    endRowIndex: number;
-  },
   wallPercentage: number,
   canvasSize: () => ISize
 ): IRenderable => {
   const cells: ICell[] = [];
   const walls: ICellElement[] = [];
   const datas: ICellPathFinderData[][] = Array(cols);
-  let path: ICellElement[] = [];
-  const drawer = Drawer();
   let cellWidth = 0;
   let cellHeight = 0;
+  const drawer = Drawer();
+  const paths: ICellElement[][] = [];
 
   return {
     setup: () => {
@@ -42,10 +63,7 @@ export const Grid = (
         datas[col] = Array(rows);
         for (let row = 0; row < rows; row++) {
           const isWall = Math.random() < wallPercentage;
-          const isStart =
-            col === init.startColIndex && row === init.startRowIndex;
-          const isTarget = col === init.endColIndex && row === init.endRowIndex;
-          if (isWall && !isStart && !isTarget) {
+          if (isWall) {
             const wall: IWall = {
               col,
               row,
@@ -75,29 +93,17 @@ export const Grid = (
         }
       }
       setNeighbors(cols, rows, datas);
-
-      const start = datas[init.startColIndex]![init.startRowIndex];
-      const end = datas[init.endColIndex]![init.endRowIndex]!;
-
-      const result = PathFinder({
-        start: start!,
-        end: end! 
-      });
-      path = result.path.map((p) => p.element);
-
-      for (const cell of result.openSet) {
-        cell.element!.types = CellType.OpenSet;
-      }
-      for (const cell of result.closeSet) {
-        cell.element!.types = CellType.CloseSet;
-      }
-
-      end.element!.types = CellType.Target;
+      paths.push(createAgent(p5, datas));
+      paths.push(createAgent(p5, datas));
+      paths.push(createAgent(p5, datas));
+      paths.push(createAgent(p5, datas));
     },
     draw: () => {
       p5.background(255);
       drawer.drawCells(p5, cells, cellWidth, cellHeight);
-      drawer.drawPath(p5, path, cellWidth, cellHeight);
+      for (const path of paths) {
+        drawer.drawPath(p5, path, cellWidth, cellHeight);
+      }
       drawer.drawWalls(p5, walls, cellWidth, cellHeight);
     },
   };
